eeb7e211947310d9381a42238c75ee3214a13119
Mariam Zhamakochyan <mariamzhamakochyan@gmail.com>
2023-07-28 09:06:57 +0400

    Update db.py

--- file-based-simple-DBMS/db.py
+++ file-based-simple-DBMS/db.py
@@ -1,25 +1,19 @@
-# simple_db.py
-
 import csv
 import os
 
-# Constants
 DB_FILE = "database.txt"
 
 def initialize_database():
-    """Create the database file if it doesn't exist."""
     if not os.path.exists(DB_FILE):
         with open(DB_FILE, "w"):
             pass
 
 def add_record(record):
-    """Add a new record to the database."""
     with open(DB_FILE, "a", newline="") as file:
         writer = csv.writer(file)
         writer.writerow(record)
 
 def read_all_records():
-    """Read all records from the database."""
     records = []
     with open(DB_FILE, "r") as file:
         reader = csv.reader(file)
@@ -28,7 +22,6 @@ def read_all_records():
     return records
 
 def update_record(record_id, updated_record):
-    """Update a record in the database."""
     records = read_all_records()
     if 0 <= record_id < len(records):
         records[record_id] = updated_record
@@ -39,7 +32,6 @@ def update_record(record_id, updated_record):
     return False
 
 def delete_record(record_id):
-    """Delete a record from the database."""
     records = read_all_records()
     if 0 <= record_id < len(records):
         del records[record_id]
